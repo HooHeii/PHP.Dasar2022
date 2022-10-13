6154310d609f592868d2c35443a16db60ddc314e
coocoo <end782@gmail.com>
2022-10-13 20:14:59 +0700

    ini nilai constant

---
+++ const.php
@@ -0,0 +1,14 @@
+<?php
+
+define ("AUTHOR", "Programmer pemula");
+define ("APP_VERSION", 100);
+
+echo "author : ";
+echo AUTHOR;
+echo "\n";
+
+
+echo "app version : ";
+echo APP_VERSION;
+echo "\n";
+
