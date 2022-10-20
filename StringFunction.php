86425071b25c633cd3e1a971cc244cb259a04fd9
coocoo <end782@gmail.com>
2022-10-20 17:37:44 +0700

    10/20/22 17:37

---
+++ StringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",", [10, 20, 30, 40]));
+var_dump(explode(",", "marq muller jack"));
+var_dump(strtolower("MARQ MARQUES JACK"));
+var_dump(strtoupper("MARQ MARQUES JACK"));
+var_dump(trim("    MARQ  MARQUES JACK     "));
+var_dump(substr("marq marques JACK", 0, 3));
